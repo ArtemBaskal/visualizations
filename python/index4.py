c219f1ff15447587b5d2d8c03dbeff351c7b35e2
ArtemBaskal <a.baskal@adguard.com>
2020-02-29 23:15:56 +0300

    refactor filters

--- python/index4.py
+++ python/index4.py
@@ -2,56 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 
+
+def draw(b, a, title):
+    w, h = signal.freqs(b, a)
+    plt.semilogx(w, 20 * np.log10(abs(h)))
+    plt.title(title)
+    plt.xlabel('Frequency [radians / second]')
+    plt.ylabel('Amplitude [dB]')
+    plt.margins(0, 0.1)
+    plt.grid(which='both', axis='both')
+    plt.show()
+    
+
 #
 b, a = signal.butter(4, 100, 'low', analog=True)
-w, h = signal.freqs(b, a)
-plt.semilogx(w, 20 * np.log10(abs(h)))
-plt.title('Butterworth filter frequency response')
-plt.xlabel('Frequency [radians / second]')
-plt.ylabel('Amplitude [dB]')
-plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
-plt.axvline(100, color='green') # cutoff frequency
-plt.show()
+draw(b, a, 'Butterworth filter frequency response')
 
 #
 b, a = signal.cheby1(4, 5, 100, 'low', analog=True)
-w, h = signal.freqs(b, a)
-plt.plot(w, 20 * np.log10(abs(h)))
-plt.xscale('log')
-plt.title('Chebyshev Type I frequency response (rp=5)')
-plt.xlabel('Frequency [radians / second]')
-plt.ylabel('Amplitude [dB]')
-plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
-plt.axvline(100, color='green')  # cutoff frequency
-plt.axhline(-5, color='green')  # rp
-plt.show()
+draw(b, a, 'Chebyshev Type I frequency response (rp=5)')
 
 #
 b, a = signal.cheby2(4, 40, 100, 'low', analog=True)
-w, h = signal.freqs(b, a)
-plt.plot(w, 20 * np.log10(abs(h)))
-plt.xscale('log')
-plt.title('Chebyshev Type II frequency response (rs=40)')
-plt.xlabel('Frequency [radians / second]')
-plt.ylabel('Amplitude [dB]')
-plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
-plt.axvline(100, color='green')  # cutoff frequency
-plt.axhline(-40, color='green')  # rs
-plt.show()
-
+draw(b, a, 'Chebyshev Type II frequency response (rs=40)')
 #
 b, a = signal.ellip(4, 5, 40, 100, 'low', analog=True)
-w, h = signal.freqs(b, a)
-plt.semilogx(w, 20 * np.log10(abs(h)))
-plt.title('Elliptic filter frequency response (rp=5, rs=40)')
-plt.xlabel('Frequency [radians / second]')
-plt.ylabel('Amplitude [dB]')
-plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
-plt.axvline(100, color='green')  # cutoff frequency
-plt.axhline(-40, color='green')  # rs
-plt.axhline(-5, color='green')  # rp
-plt.show()
+draw(b, a, 'Elliptic filter frequency response (rp=5, rs=40)')
