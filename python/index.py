6d7f5618a1d02255f656a13fa50a299aba13a3a8
ArtemBaskal <a.baskal@adguard.com>
2020-02-29 11:19:58 +0300

    Update two python scripts

--- python/index.py
+++ python/index.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import rfft, irfft, rfftfreq
+from scipy.fftpack import fft, ifft, rfftfreq
 from math import pi
 from numpy import cos
-import scipy.signal as sig
 
 A = 10
 B = 2
@@ -11,9 +10,8 @@ f1 = 2000
 f2 = 150
 
 Fs = 40000
-time = 0.01
 
-t = np.arange(0, time, 1. / Fs)
+t = np.arange(0, 300. / Fs, 1. / Fs)
 n = np.size(t)
 fr = rfftfreq(n, 1. / Fs)
 
@@ -70,14 +68,14 @@ def draw_signal_graph(s, title='Signal', xlabel='Time, s'):
     draw_graph(t, s, title, xlabel)
 
 
-def draw_spectrum_graph(s, title='Spectrum', xlabel='Frequency, Hz'):
-    spectr = abs(rfft(s)) / n
-    draw_graph(fr, spectr, title, xlabel)
+def draw_spectrum_graph(spectrum, signal_title='Signal', xlabel='Frequency, Hz'):
+    draw_graph(2 * fr, spectrum, f"Spectrum of {signal_title}", xlabel)
 
 
 def draw_signal_and_spectrum(signal, signal_title):
     draw_signal_graph(signal, signal_title)
-    draw_spectrum_graph(signal, f"Spectrum of {signal_title}")
+    spectrum = abs(fft(signal))
+    draw_spectrum_graph(spectrum, signal_title)
 
 
 def add_noise(signal):
@@ -97,19 +95,14 @@ def task2(fm_signal, noised_signal):
 
 
 def task3(noised_signal):
-    o = 5
-    fc = np.array([1, 10000])
-    wc = 2 * fc / Fs
-    [b, a] = sig.butter(o, wc, btype="bandpass")
-
-    filtered_signal = sig.lfilter(b, a, noised_signal)
-    draw_signal_and_spectrum(filtered_signal, 'Filtered AM-signal')
-    return filtered_signal
+    spectrum = abs(fft(noised_signal))
+    spectrum[spectrum < 200] = 0
+    draw_spectrum_graph(spectrum, 'Filtered AM-signal')
+    return spectrum
 
 
-def task4(filtered_signal):
-    spectrum = rfft(filtered_signal)
-    filtered_signal = irfft(spectrum)
+def task4(spectrum):
+    filtered_signal = ifft(spectrum).real
     draw_signal_and_spectrum(filtered_signal, 'IFT AM-signal')
 
 
@@ -121,5 +114,5 @@ pure_fm_signal = generate_fm_signal(amp_c=A, amp_s=B, fc=f1, fs=f2)
 noised_fm_signal = add_noise(pure_fm_signal)
 task2(pure_fm_signal, noised_fm_signal)
 
-filtered_am_signal = task3(noised_am_signal)
-task4(filtered_am_signal)
+sig = task3(noised_am_signal)
+task4(sig)
--- python/index2.py
+++ python/index2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.fftpack import rfft, irfft
+import math
 
 
 def draw_graphs(freq_list, title_list=['', '', '']):
@@ -26,6 +27,11 @@ def circle_convolve(signal1, signal2):
     return irfft(rfft(signal1) * rfft(signal2)) / np.sum(signal1)
 
 
+def sum_of_correlation(signal1, signal2):
+    return np.array(
+        [np.sum(signal1 * np.roll(signal2, i)) for i in range(math.floor(0.5 * (len(signal1) + len(signal2))))])
+
+
 def draw_convolve(signal1, signal2, title_list=['Signal', 'Signal']):
     title_list.append('Convolution')
     convolution = convolve(signal1, signal2)
@@ -35,6 +41,12 @@ def draw_convolve(signal1, signal2, title_list=['Signal', 'Signal']):
 def draw_circle_convolve(signal1, signal2, title_list=['Signal', 'Signal']):
     title_list.append('Circle Convolution')
     convolution = circle_convolve(signal1, signal2)
+    draw_graphs([signal1, signal2, convolution], title_list)
+
+
+def draw_convolve_2(signal1, signal2, title_list=['Signal', 'Signal']):
+    title_list.append('Convolution')
+    convolution = sum_of_correlation(signal1, signal2)
     draw_graphs([signal1, signal2, convolution], title_list)
 
 
@@ -58,9 +70,16 @@ def task3(square_wave, triangular_wave):
     draw_circle_convolve(square_wave, triangular_wave, ['Square', 'Triangular'])
 
 
+def task4(square_wave, triangular_wave):
+    draw_convolve_2(square_wave, square_wave, ['Square', 'Square'])
+    draw_convolve_2(triangular_wave, triangular_wave, ['Triangular', 'Triangular'])
+    draw_convolve_2(square_wave, triangular_wave, ['Square', 'Triangular'])
+
+
 square_wave = np.repeat([0., 1., 0.], 200)
 triangular_wave = convolve(square_wave, square_wave)
 
 task1(square_wave, triangular_wave)
 task2(square_wave)
 task3(square_wave, triangular_wave)
+task4(square_wave, triangular_wave)
