01c44ae155ec4e628d077701df27b5f48aa80602
ArtemBaskal <a.baskal@adguard.com>
2020-03-01 13:22:54 +0300

    add highpass filters graphs

--- python/index4.py
+++ python/index4.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 
+Fs = 44100
+
+fc = np.array([1, 10000])
+wc = 2 * fc / Fs
+
 
 def draw(x, y, title, xlabel, ylabel):
     plt.semilogx(x, y)
@@ -21,26 +26,36 @@ def draw_graphs(b, a, title):
 
 def draw_frequency_response(w, h, title):
     draw(w, 20 * np.log10(abs(h)),
-          f'{title} frequency response',
-          'Frequency [radians / second]',
-          'Amplitude [dB]')
+         f'{title} frequency response',
+         'Frequency [radians / second]',
+         'Amplitude [dB]')
 
 
 def draw_phase_response(w, h, title):
     draw(w, np.unwrap(np.angle(h)),
-          f'{title} phase response',
-          'Frequency [radians / second]',
-          'Phase [radians]')
+         f'{title} phase response',
+         'Frequency [radians / second]',
+         'Phase [radians]')
 
 
-b, a = signal.butter(4, 100, 'low', analog=True)
-draw_graphs(b, a, 'Butterworth filter')
+def butter(ftype):
+    b, a = signal.butter(5, 100, ftype, analog=True)
+    draw_graphs(b, a, f'{ftype} Butterworth filter')
 
-b, a = signal.cheby1(4, 5, 100, 'low', analog=True)
-draw_graphs(b, a, 'Chebyshev Type I (rp=5)')
 
-b, a = signal.cheby2(4, 40, 100, 'low', analog=True)
-draw_graphs(b, a, 'Chebyshev Type II (rs=40)')
+def cheby1(ftype):
+    b, a = signal.cheby1(4, 5, 100, ftype, analog=True)
+    draw_graphs(b, a, f'{ftype} Chebyshev Type I (rp=5)')
 
-b, a = signal.ellip(4, 5, 40, 100, 'low', analog=True)
-draw_graphs(b, a, 'Elliptic filter (rp=5, rs=40)')
+
+def cheby2(ftype):
+    b, a = signal.cheby2(4, 40, 100, ftype, analog=True)
+    draw_graphs(b, a, f'{ftype} Chebyshev Type II (rs=40)')
+
+
+def ellip(ftype):
+    b, a = signal.ellip(4, 5, 40, 100, ftype, analog=True)
+    draw_graphs(b, a, f'{ftype} Elliptic filter (rp=5, rs=40)')
+
+
+list(map(lambda f: list(map(f, ['lowpass', 'highpass'])), [butter, cheby1, cheby2, ellip]))
