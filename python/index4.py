b8a1888a4d6a4c98dede32c4728a3e2c79c09de5
ArtemBaskal <a.baskal@adguard.com>
2020-03-01 22:25:09 +0300

    add group of curves

--- python/index4.py
+++ python/index4.py
@@ -7,55 +7,124 @@ Fs = 44100
 fc = np.array([1, 10000])
 wc = 2 * fc / Fs
 
+Btypes = {
+    'lowpass': 'lowpass',
+    'highpass': 'highpass',
+    'bandpass': 'bandpass',
+    'bandstop': 'bandstop'
+}
 
-def draw(x, y, title, xlabel, ylabel):
+
+def draw(x, y, title, xlabel, ylabel, show=True):
     plt.semilogx(x, y)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.margins(0, 0.1)
     plt.grid(which='both', axis='both')
-    plt.show()
+    if show:
+        plt.show()
 
 
-def draw_graphs(b, a, title):
+def draw_graphs(b, a, title, show=True):
     w, h = signal.freqs(b, a)
-    draw_frequency_response(w, h, title)
-    draw_phase_response(w, h, title)
+    draw_frequency_response(w, h, title, show)
+    draw_phase_response(w, h, title, show)
 
 
-def draw_frequency_response(w, h, title):
+def draw_frequency_response(w, h, title, show=True):
     draw(w, 20 * np.log10(abs(h)),
          f'{title} frequency response',
          'Frequency [radians / second]',
-         'Amplitude [dB]')
+         'Amplitude [dB]', show)
 
 
-def draw_phase_response(w, h, title):
+def draw_phase_response(w, h, title, show=True):
     draw(w, np.unwrap(np.angle(h)),
          f'{title} phase response',
          'Frequency [radians / second]',
-         'Phase [radians]')
+         'Phase [radians]', show)
 
 
-def butter(ftype):
-    b, a = signal.butter(5, 100, ftype, analog=True)
-    draw_graphs(b, a, f'{ftype} Butterworth filter')
+def butter(btype="low", N=4, Wn=100, show=True, task=1):
+    if (btype == Btypes['lowpass'] or btype == Btypes['highpass']):
+        Wn = 100
+    if (btype == Btypes['bandpass'] or btype == Btypes['bandstop']):
+        Wn = wc
+
+    b, a = signal.butter(N, Wn, btype, analog=True)
+
+    title = f'{btype} Butterworth filter'
+    if task == 1:
+        draw_graphs(b, a, title, show)
+    if task == 2:
+        w, h = signal.freqs(b, a)
+        draw_frequency_response(w, h, title, show=False)
+    if task == 3:
+        print(task)
 
 
-def cheby1(ftype):
-    b, a = signal.cheby1(4, 5, 100, ftype, analog=True)
-    draw_graphs(b, a, f'{ftype} Chebyshev Type I (rp=5)')
+def cheby1(btype="low", N=4, Wn=100, show=True, task=1):
+    if (btype == Btypes['lowpass'] or btype == Btypes['highpass']):
+        Wn = 100
+    if (btype == Btypes['bandpass'] or btype == Btypes['bandstop']):
+        Wn = wc
+
+    b, a = signal.cheby1(N, 5, Wn, btype, analog=True)
+
+    title = f'{btype} Chebyshev Type I (rp=5)'
+    if task == 1:
+        draw_graphs(b, a, title, show)
+    if task == 2:
+        w, h = signal.freqs(b, a)
+        draw_frequency_response(w, h, title, show=False)
+    if task == 3:
+        print(task)
 
 
-def cheby2(ftype):
-    b, a = signal.cheby2(4, 40, 100, ftype, analog=True)
-    draw_graphs(b, a, f'{ftype} Chebyshev Type II (rs=40)')
+def cheby2(btype="low", N=4, Wn=100, show=True, task=1):
+    if (btype == Btypes['lowpass'] or btype == Btypes['highpass']):
+        Wn = 100
+    if (btype == Btypes['bandpass'] or btype == Btypes['bandstop']):
+        Wn = wc
+
+    b, a = signal.cheby2(N, 40, Wn, btype, analog=True)
+
+    title = f'{btype} Chebyshev Type II (rs=40)'
+    if task == 1:
+        draw_graphs(b, a, title, show)
+    if task == 2:
+        w, h = signal.freqs(b, a)
+        draw_frequency_response(w, h, title, show=False)
+    if task == 3:
+        draw_graphs(b, a, title, show)
 
 
-def ellip(ftype):
-    b, a = signal.ellip(4, 5, 40, 100, ftype, analog=True)
-    draw_graphs(b, a, f'{ftype} Elliptic filter (rp=5, rs=40)')
+def ellip(btype="low", N=4, Wn=100, show=True, task=1):
+    if (btype == Btypes['lowpass'] or btype == Btypes['highpass']):
+        Wn = 100
+    if (btype == Btypes['bandpass'] or btype == Btypes['bandstop']):
+        Wn = wc
+
+    b, a = signal.ellip(N, 5, 40, Wn, btype, analog=True)
+
+    title = f'{btype} Elliptic filter (rp=5, rs=40)'
+    if task == 1:
+        draw_graphs(b, a, title, show)
+    if task == 2:
+        w, h = signal.freqs(b, a)
+        draw_frequency_response(w, h, title, show=False)
+    if task == 3:
+        print(task)
 
 
-list(map(lambda f: list(map(f, ['lowpass', 'highpass'])), [butter, cheby1, cheby2, ellip]))
+def draw_group(f):
+    order = range(1, 11)
+    list(map(lambda i: f(N=i, show=False, task=2), order))
+    plt.legend(order)
+    plt.show()
+
+
+list(map(lambda f: list(map(lambda i: f(i), list(Btypes))), [butter, cheby1, cheby2, ellip]))
+
+list(map(draw_group, [butter, cheby1, cheby2, ellip]))
